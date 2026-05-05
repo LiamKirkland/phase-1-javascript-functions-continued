4f21da84a1f9288a5d17195d1e6ecfc560068b51
LiamKirkland <liam.b.kirkland@gmail.com>
2026-05-05 09:13:14 -0400

    Function Practice pt2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flare="*") {
+    function inner(desc = "special") {
+        return `You are ${flare}${desc}${flare}!`
+    }
+    return inner
+}
+
+const encouragingMessage = wrapAdjective("<3")("super cool")
